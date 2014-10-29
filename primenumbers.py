0f9a130bafcdf90629a0686d806eaea5a513c0f0
daajdk <mccarthyk1@students.rowan.edu>
2014-10-29 13:08:23 -0400

    Primes with error checking

--- primenumbers.py
+++ primenumbers.py
@@ -2,7 +2,7 @@
 #Prime Number calculator
 #calculates primes numbers up to N
 import math
- 
+
 def isPrime(num):
     for k in range(2, num):
         if (num % k) == 0:
@@ -11,12 +11,21 @@ def isPrime(num):
 
 def main():
     inputNum = int(input("What is the maximum number?: "))
-    
-    primeList = []
-    primeList.append(2) #2 is always prime
-    
-    for x in range(3, inputNum):
-        if (isPrime(x)):
-            primeList.append(x)
 
-    print(primeList)
+    while (inputNum < 1):
+        print("Enter a number greater than 0")
+        inputNum = int(input("Reenter number: ")) 
+    
+    if (inputNum == 1):
+        primeList = [1]
+        print(primeList)
+    else:
+        primeList = []
+        primeList.append(2) #2 is always prime
+
+        for x in range(3, inputNum):
+            if (isPrime(x)):
+                primeList.append(x)
+    
+        print(primeList)
+
