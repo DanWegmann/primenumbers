00fb1d272b43328bc84192afd65db938f5a9ace4
daajdk <mccarthyk1@students.rowan.edu>
2014-10-29 12:29:50 -0400

    Primes with comments

--- primenumbers.py
+++ primenumbers.py
@@ -1,16 +1,19 @@
+#Dan Wegmann
+#Prime Number calculator
+#calculates primes numbers up to N
 import math
  
 def isPrime(num):
     for k in range(2, num):
         if (num % k) == 0:
-            return False
-    return True
+            return False #number is not prime
+    return True #number is prime
 
 def main():
     inputNum = int(input("What is the maximum number?: "))
     
     primeList = []
-    primeList.append(2)
+    primeList.append(2) #2 is always prime
     
     for x in range(3, inputNum):
         if (isPrime(x)):
