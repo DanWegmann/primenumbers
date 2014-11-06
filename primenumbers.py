9892084da761e3d3577a4d799fbcc1f661977d94
nmccler89 <mccler89@students.rowan.edu>
2014-11-06 00:01:48 -0500

    Primes with changes
    Removed check for 1, 1 is not a prime
    Removed check for 2, 2 will show up prime and does not need a special
    case
    Added +1 to range for going from 2 to num, allows inclusion of input
    number
    took duplicate print list out so it only prints once

--- primenumbers.py
+++ primenumbers.py
@@ -1,31 +1,32 @@
-##Dan Wegmann
+## Dan Wegmann
 #Prime Number calculator
 #calculates primes numbers up to N
 import math
+ 
 
 def isPrime(num):
     for k in range(2, num):
-        if (num % k) == 0:
-            return False #number is not prime
+        if (num % k) == 0: return False #number is not prime
     return True #number is prime
+
 
 def main():
     inputNum = int(input("What is the maximum number?: "))
 
+    primeList = []
+
     while (inputNum < 1):
         print("Enter a number greater than 0")
         inputNum = int(input("Reenter number: ")) 
-    
-    if (inputNum == 1):
-        primeList = [1]
-        print(primeList)
-    else:
-        primeList = []
-        primeList.append(2) #2 is always prime
 
-        for x in range(3, inputNum):
-            if (isPrime(x)):
-                primeList.append(x)
-    
-        print(primeList)
+    # ranged(x,y), x is inclusive, y is exclusive so use y + 1
+    # will check if number entered is prime in addition to all numbers lower than it
+    # 2 should come up prime without needing a special case
+    # 1 is not prime
+    for x in range(2, inputNum + 1):
+        if (isPrime(x)):
+            primeList.append(x)
 
+    print(primeList)
+
+main()
